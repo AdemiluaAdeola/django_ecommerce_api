829ea9eabbfa69237ad71e2c9f558121a8311d21
AdemiluaAdeola <ademiluaadeola969@gmail.com>
2023-08-17 01:17:06 +0100

    completed

---
+++ store_app/filters.py
@@ -0,0 +1,11 @@
+from django_filters.rest_framework import FilterSet
+from store_app.models import Product
+
+
+class ProductFilter(FilterSet):
+    class Meta:
+        model = Product
+        fields = {
+            'category_id': ['exact'],
+            'price': ['gt', 'lt']
+        }
